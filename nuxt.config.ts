5b44928fe9ee2ce1b296d8079b375b4c975a6f41
pedro.nogueira <pedro.crnogueira@gmail.com>
2025-01-29 17:05:53 -0300

    Testeing configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     baseURL: "/portifolio/",
   },
+
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/assets/main.css"],
 });
