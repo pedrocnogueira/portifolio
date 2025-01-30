f2304215a962bde538401916a656ca9478b3bb3f
pedro.nogueira <pedro.crnogueira@gmail.com>
2025-01-30 12:27:01 -0300

    Adding the main content and core HTML structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from "path";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -9,12 +8,15 @@ export default defineNuxtConfig({
     baseURL: "/portifolio/",
   },
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
   alias: {
     "@": resolve(__dirname, "/"),
+    "@assets": resolve(__dirname, "assets"),
   },
   css: ["~/assets/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
